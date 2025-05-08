1367f438331ed9a37a9f0693744cfa6ef8eb86ac
Trevor Freed <tfreed804@gmail.com>
2025-05-07 23:13:13 -0400

    v0.2: removed an unneccessary library import

--- rps.py
+++ rps.py
@@ -1,11 +1,19 @@
 #This is my first fully independent project - Rock, Paper, Scissors
-import random
-import sys
 
+#import required libraries
+import random
+
+#The main function will contain the game loop
 def main():
+
+    #Begin the loop
     while True:
+
+        #Ask the user for the number of players for this game session
         num_players = input("Are there 1 or 2 players? ")
-    
+
+        #Validate user input. If valid, ask for player name(s)
+        #If invalid, ask the user again.
         if num_players == "2":
             player1 = input("Enter the name for player 1: ")
             player2 = input("Enter the name for player 2: ")
@@ -15,6 +23,10 @@ def main():
         else:
             print("This was not valid input! Try again.")
             continue
+
+
+
+
 
         
 main()
